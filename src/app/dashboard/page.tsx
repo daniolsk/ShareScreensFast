8e8bcba4fead3dbb25320f556043f7a33742f3cd
daniolsk <danio.skowron@gmail.com>
2024-05-12 14:11:25 +0200

    fix layout on mobile

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -15,7 +15,7 @@ export default async function page({
   return (
     <main className="flex-1">
       <div className="flex-1 p-4 md:p-6">
-        <div className="grid max-h-screen grid-cols-2 items-center justify-items-center gap-4 sm:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6">
+        <div className="grid grid-cols-2 items-center justify-items-center gap-4 sm:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6">
           {userImages.map((image) => (
             <ImageComponent
               key={image.id}
